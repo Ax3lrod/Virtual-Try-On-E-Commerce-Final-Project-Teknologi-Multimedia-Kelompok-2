c96ebc9f595f887998c70555c868863c069ea6a9
Ax3lrod <aryasatyagigachad9@gmail.com>
2025-06-19 12:48:59 +0700

    fix: fix async error

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -2,7 +2,7 @@ import { products } from "@/content/products";
 import { notFound } from "next/navigation";
 import ProductDetail from "@/components/ProductDetail";
 
-export default function ProductPage({ params }: { params: { id: string } }) {
+export default async function ProductPage({ params }: { params: { id: string } }) {
   const product = products.find((p) => p.id === params.id);
   if (!product) return notFound();
 
